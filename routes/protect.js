c47f71dda9417c6b161e412bf422b06f7a43262d
Yash Bansal <agarwaly482@gmail.com>
2021-03-19 01:06:52 +0530

    added qrcode routes

---
+++ routes/protect.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router(),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    localStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("../models/user");
+
+// mongoose.connect(process.env.LOCALDB, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+//     console.log("db connected in protect route");
+// });
+
+router.use(require("express-session")({
+    secret: "secret!",
+    resave: false,
+    saveUninitialized: false
+}));
+router.use(passport.initialize());
+router.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+router.use(bodyParser.urlencoded({extended: true}));
+
+router.get("/",isLoggedIn,(req,res)=>{
+    res.render("child")
+})
+
+function isLoggedIn(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
